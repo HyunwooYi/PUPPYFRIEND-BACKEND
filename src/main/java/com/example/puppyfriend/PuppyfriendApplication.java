cd0fa1ff5c39c68cd7d7a0e734b5814ca07fbdc4
MJ054 <forgit0504@gmail.com>
2023-07-29 16:54:22 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/java/com/example/puppyfriend/PuppyfriendApplication.java
@@ -0,0 +1,13 @@
+package com.example.puppyfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyfriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/puppyfriend/PuppyfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puppyfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
