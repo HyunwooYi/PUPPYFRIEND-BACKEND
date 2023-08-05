45533799cd21afb46dfb7f0fe85f42ac303a2f65
SongKong00 <hesong1@naver.com>
2023-08-05 18:39:40 +0900

    Fix: SnsPhoto 및 Sns 패키지 위치 이동, SnsCategory 내용 변경

---
+++ src/main/java/com/example/puppyfriend/sns/domain/SnsPhoto.java
@@ -0,0 +1,34 @@
+package com.example.puppyfriend.domain;
+
+import com.example.puppyfriend.sns.domain.Sns;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SnsPhoto {
+
+    @Id
+    @GeneratedValue
+    private int sns_photo_id;
+
+    @OneToOne
+    @JoinColumn(name = "sns_id", nullable = false)  //Photo는 Post와 연관이 맺어져있을 경우에만 저장될 수 있도록, nullable=false로 설정
+    @OnDelete(action = OnDeleteAction.CASCADE)     //게시글이 제거되면 이미지도 연쇄적으로 제거될 수 있도록, @OnDelete를 지정
+    private Sns sns;
+
+    @Column(length = 45)
+    private String imageUrl;
+
+    @Builder
+    public SnsPhoto(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+}
