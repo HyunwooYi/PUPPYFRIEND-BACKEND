201e1c584bedb73eb52758219ad7e09b0b366778
김예린 <yrkim6228@duksung.ac.kr>
2023-08-19 23:39:48 +0900

    Feat:ApiOperation 추가

--- src/main/java/com/example/puppyfriend/user/controller/UserController.java
+++ src/main/java/com/example/puppyfriend/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.puppyfriend.user.dto.UserJoinDto;
 import com.example.puppyfriend.user.service.UserService;
 import com.example.puppyfriend.util.BaseException;
 import com.example.puppyfriend.util.BaseResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ public class UserController {
 
     private final UserService userService;
     @PostMapping("/login")
+    @ApiOperation(value="로그인", notes="로그인 api 입니다. \n" +  "id와 password을 입력하면 로그인 성공/실패 여부를 알 수 있습니다. ")
     public BaseResponse<String> login(UserDto userDto) {
         try{
             //userService.login(userDto.getName(), userDto.getPassword());
@@ -30,6 +32,9 @@ public class UserController {
     }
 
     @PostMapping("/join")
+    @ApiOperation(value="회원가입", notes="회원가입 api 입니다. \n" + "birth: ex)2000-06-17 형식으로 주셔야 합니다. \n " +
+            "gender: true -> 남자 / false -> 여자입니다. \n " +
+            "marketing: 선택 약관(마케팅 동의)에 체크했다면 true로 보내주세요 :) \n")
     public BaseResponse<String> join(@RequestBody UserJoinDto userJoinDto) {
         try{
             userService.createUser(userJoinDto);
